a094185c88c6cd7ce3495b9066bd52e086754dc0
IlyaBulatau <ilyabulatau@gmail.com>
2023-10-15 18:27:32 +0200

    added the handlers and playing with than

--- ptb/main.py
+++ ptb/main.py
@@ -22,6 +22,17 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="OK")
 
 
+async def info_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=" ".join(context.args))
+        return
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Not args")
+
+
+async def unknow_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Unknow message, sorry")
+
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
@@ -29,6 +40,8 @@ if __name__ == "__main__":
     token = Settings.token
     app = ApplicationBuilder().token(token).build()
 
-    app.add_handler(CommandHandler(command='start', callback=start_command))
+    app.add_handler(CommandHandler(command="start", callback=start_command))
+    app.add_handler(CommandHandler(command="info", callback=info_command))
+    app.add_handler(MessageHandler(filters=filters.COMMAND, callback=unknow_command))
     app.add_handler(MessageHandler(filters=filters.TEXT, callback=echo))
     app.run_polling()
