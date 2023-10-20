01e2af60cdb35673e49352327d6002ed3ef30f46
IlyaBulatau <ilyabulatau@gmail.com>
2023-10-20 16:39:38 +0200

    Add postgresql connect data
    Add login, password, port, host, name to .env file and varibles in config.py file
    These are data for connect to postgresql databases

--- ptb/config.py
+++ ptb/config.py
@@ -6,3 +6,11 @@ class Settings:
     env.read_env()
 
     token = env("TOKEN")
+
+    _db_login = env("POSTGRES_LOGIN")
+    _db_password = env("POSTGRES_PASSWORD")
+    _db_port = env("POSTGRES_PORT")
+    _db_host = env("POSTGRES_HOST")
+    _db_name = env("POSTGRES_NAME")
+
+    db_url = f"asyncpg+postgresql://{_db_login}:{_db_password}@{_db_host}:{_db_port}/{_db_name}"
