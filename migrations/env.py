0dbb60052cbc080dca75503383b7c40f31b4ba6d
IlyaBulatau <ilyabulatau@gmail.com>
2023-10-22 15:35:24 +0200

    Clear migration
    Clear all migrations, that allow create correct first migration

--- migrations/env.py
+++ migrations/env.py
@@ -8,8 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from ptb.config import Settings
-from ptb.models import Base, User
-
+from ptb.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
